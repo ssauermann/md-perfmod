07f02e89a989df2397a2c0b14729a991cf1df237
Sascha Sauermann <sascha.sauermann@tum.de>
2018-06-10 14:36:45 +0200

    Creating models dynamically

--- visualizer/app.py
+++ visualizer/app.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
+import subprocess
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import os
 import pandas as pd
 import plotly.graph_objs as go
+import tempfile
 from dash.dependencies import Input, Output
 from functools import partial
+import re
+from flask_caching import Cache
 
-df = pd.read_csv('../ls1-bench1.csv')
+csv_file_path = os.path.relpath('../ls1-bench1.csv')
+df = pd.read_csv(csv_file_path)
 
 app = dash.Dash()
+cache = Cache(app.server, config={'CACHE_TYPE': 'simple'})
 
 sliders = []
 slider_names = []
@@ -72,7 +80,6 @@ app.layout = html.Div(children=[
             dcc.Dropdown(
                 id='sel_compare',
                 options=list(map(lambda c: {'label': c, 'value': c}, selectable_columns)),
-                value=selectable_columns[1]
             )
         ], style={'width': '25%', 'float': 'left', 'display': 'inline-block'}),
     ]),
@@ -86,6 +93,8 @@ app.layout = html.Div(children=[
 
     html.Div([
         html.H3('Model'),
+        dcc.Graph(id='model-graph'),
+        html.P(id='model'),
     ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
 
     html.Div([], style={'margin-top': '4em'})
@@ -102,7 +111,70 @@ def generate_slider_updates():
             [Input('sel_var1', 'value'), Input('sel_compare', 'value'), Input('sel_repeat', 'value')]
         )(partial(update_slider, slider_col))
 
+
 generate_slider_updates()
+
+
+#@app.callback(Output('model-graph', 'figure'),
+#              [Input('sel_var1', 'value'), Input('sel_metric', 'value'),
+#               Input('sel_compare', 'value'), Input('sel_repeat', 'value'), Input('model', 'children')]
+#              + [Input(sid, 'value') for sid in slider_names])
+#def update_model_graph(sel_var1, sel_metric, sel_compare, sel_repeat, model, *args):
+    #pass
+
+@app.callback(Output('model', 'children'),
+              [Input('sel_var1', 'value'), Input('sel_metric', 'value'),
+               Input('sel_compare', 'value'), Input('sel_repeat', 'value')]
+              + [Input(sid, 'value') for sid in slider_names])
+@cache.memoize()
+def update_model(sel_var1, sel_metric, sel_compare, sel_repeat, *args):
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../csv2extrap.py')
+
+    # prevent variable and metric to be null
+    if sel_var1 is None:
+        sel_var1 = selectable_columns[0]
+    if sel_metric is None:
+        sel_metric = metric_columns[0]
+
+    def create_model(additional_filters=list()):
+        try:
+            _, tmp_file_in = tempfile.mkstemp()
+            call_params = ['python3', path, csv_file_path, tmp_file_in, '-v', sel_var1, '-m', sel_metric]
+
+            # handle fixed columns
+            filters = []
+            for col, val in zip(selectable_columns, args):
+                if col not in [sel_var1, sel_repeat, sel_compare]:
+                    filters.append("%s=%s" % (col, val))
+
+            call_params += ['-f', " ".join(filters + additional_filters)]
+
+            # handle repeat column
+            if sel_repeat is None:
+                call_params.append('--single-measurement')
+            else:
+                call_params += ['-r', sel_repeat]
+
+            subprocess.check_call(call_params)
+
+            _, tmp_file_out = tempfile.mkstemp()
+            subprocess.check_call(['/opt/extrap/bin/extrap-modeler', 'input', tmp_file_in, '-o', tmp_file_out])
+            model_summary = subprocess.check_output(['/opt/extrap/bin/extrap-print', tmp_file_out]).decode("utf-8")
+
+            print(model_summary)
+
+            return re.search(r'model: (.+)\n', model_summary).group(1)
+        except subprocess.CalledProcessError:
+            return None
+
+    models = []
+    if sel_compare is None:
+        models.append(create_model())
+    else:
+        for cmp_val in df[sel_compare].unique():
+            models.append(create_model(['%s=%s' % (sel_compare, cmp_val)]))
+
+    return '; '.join(models)
 
 
 @app.callback(Output('1d-graph', 'figure'),
@@ -110,37 +182,30 @@ generate_slider_updates()
                Input('sel_compare', 'value'), Input('sel_repeat', 'value')]
               + [Input(sid, 'value') for sid in slider_names])
 def update_figure(sel_var1, sel_metric, sel_compare, sel_repeat, *args):
-    # sliders[selectable_columns.index(sel_var1)].disabled = True
-
     # filtering
     filtered_df = df
     for col, val in zip(selectable_columns, args):
         if col in [sel_var1, sel_metric, sel_compare, sel_repeat]:
             continue
-
-        print(col, val)
         filtered_df = filtered_df[df[col] == val]
 
-    print(filtered_df)
-
     if sel_var1 is None:
-        sel_var1=selectable_columns[0]
+        sel_var1 = selectable_columns[0]
     if sel_metric is None:
-        sel_metric=metric_columns[1]
+        sel_metric = metric_columns[1]
 
     data_list = []
-    mode = 'lines+markers' if sel_repeat is None else 'markers'
+    mode = 'lines+markers' if sel_repeat is None else 'markers'  # TODO Median line if showing multiple repeats?
 
     if sel_compare is not None:
         split_dfs = [frame for frame in filtered_df.groupby(sel_compare)]
-
 
         for region, frame in split_dfs:
             d = go.Scatter(
                 x=frame[sel_var1],
                 y=frame[sel_metric],
                 mode=mode,
-                name=region,
+                name=sel_compare + ': ' + str(region),
             )
             data_list.append(d)
     else:

654275c6ef1a8d6ad4513d857fdf990ea7ce1c77
Sascha Sauermann <sascha.sauermann@tum.de>
2018-06-16 00:47:23 +0200

    Using wire mesh visualization instead of surfaces.

--- md_perfmod/visualizer/graphs.py
+++ md_perfmod/visualizer/graphs.py
@@ -1,6 +1,5 @@
-from itertools import product
-
 import colorlover as cl
+import numpy as np
 import plotly.graph_objs as go
 
 
@@ -63,25 +62,42 @@ def one_d_graph_multi(models, bounds, filtered_df, sel_var1, sel_metric, sel_com
 
 def two_d_graph(models, bounds, filtered_df, sel_var1, sel_var2, sel_metric):
     x, samples = models[0].sample(*bounds)
-    samples = samples.reshape(-1)
-    x, y = zip(*product(x[0], x[1]))
     options_m = dict(
-        x=x,
-        y=y,
-        z=samples,
         name='model',
-        showscale=False,
-        opacity=0.9,
+        showlegend=True,
+        mode='lines',
+        line=dict(color='#1f77b4', width=3),
     )
     options_d = dict(
         x=filtered_df[sel_var1],
         y=filtered_df[sel_var2],
         z=filtered_df[sel_metric],
         mode='markers',
+        marker=dict(color='#ff7f0e'),
         name='data',
     )
 
-    return [go.Mesh3d(options_m), go.Scatter3d(options_d)]
+    return create_mesh(x, samples, options_m) + [go.Scatter3d(options_d)]
+
+
+def create_mesh(x, samples, options_m):
+    data_list = []
+    x_grid, y_grid = np.meshgrid(x[0], x[1])
+    for a, b, c in zip(x_grid, y_grid, samples):
+        options_m['x'] = a
+        options_m['y'] = b
+        options_m['z'] = c
+        data_list.append(go.Scatter3d(options_m))
+        options_m['showlegend'] = False
+
+    y_grid, x_grid = np.meshgrid(x[1], x[0])
+    for a, b, c in zip(x_grid, y_grid, samples.T):
+        options_m['x'] = a
+        options_m['y'] = b
+        options_m['z'] = c
+        data_list.append(go.Scatter3d(options_m))
+
+    return data_list
 
 
 def two_d_graph_multi(models, bounds, filtered_df, sel_var1, sel_var2, sel_metric, sel_compare):
@@ -93,15 +109,11 @@ def two_d_graph_multi(models, bounds, filtered_df, sel_var1, sel_var2, sel_metri
         name = str(region)
         model = next(m for m in models if str(m.name) == name)
         x, samples = model.sample(*bounds)
-        samples = samples.reshape(-1)
-        x, y = zip(*product(x[0], x[1]))
         options_m = dict(
-            x=x,
-            y=y,
-            z=samples,
             name='%s: %s (model)' % (sel_compare, name),
+            mode='lines',
+            line=dict(width=2),
             legendgroup=region,
-            opacity=1,
         )
 
         options_d = dict(
@@ -115,13 +127,13 @@ def two_d_graph_multi(models, bounds, filtered_df, sel_var1, sel_var2, sel_metri
 
         if 3 < num_colors < 13:
             colors = cl.scales[str(num_colors)]['qual']['Paired']
-            options_m['color'] = colors[2 * i]
+            options_m['line'] = dict(color=colors[2 * i], width=3)
             options_d['marker'] = dict(color=colors[2 * i + 1])
         elif num_colors < 25:
             colors = cl.scales[str(num_colors)]['qual']['Set1']
-            options_m['color'] = colors[i]
+            options_m['line'] = dict(color=colors[i], width=3)
             options_d['marker'] = dict(color=colors[i])
 
-        data_list += [go.Mesh3d(options_m), go.Scatter3d(options_d)]
+        data_list += [go.Scatter3d(options_d)] + create_mesh(x, samples, options_m)
 
     return data_list
